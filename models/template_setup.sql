91dc80caa6c74df1f2ee3256b6f71b48005d17b7
brandon <wingo206bc@gmail.com>
2024-03-18 17:00:32 -0400

    added all sql users and connection pools

--- models/template_setup.sql
+++ models/template_setup.sql
@@ -5,8 +5,17 @@ create database {{database}};
 drop user if exists '{{sqlAuthUser}}'@'{{sqlHost}}';
 create user '{{sqlAuthUser}}'@'{{sqlHost}}' identified by '{{sqlAuthPw}}';
 
+drop user if exists '{{sqlVisitorUser}}'@'{{sqlHost}}';
+create user '{{sqlVisitorUser}}'@'{{sqlHost}}' identified by '{{sqlVisitorPw}}';
+
 drop user if exists '{{sqlCustomerUser}}'@'{{sqlHost}}';
 create user '{{sqlCustomerUser}}'@'{{sqlHost}}' identified by '{{sqlCustomerPw}}';
+
+drop user if exists '{{sqlEmployeeUser}}'@'{{sqlHost}}';
+create user '{{sqlEmployeeUser}}'@'{{sqlHost}}' identified by '{{sqlEmployeePw}}';
+
+drop user if exists '{{sqlAdminUser}}'@'{{sqlHost}}';
+create user '{{sqlAdminUser}}'@'{{sqlHost}}' identified by '{{sqlAdminPw}}';
 
 use {{database}};
 
@@ -71,6 +80,9 @@ primary key(mid, store_id),
 foreign key(mid) references menu_item(mid),
 foreign key(store_id) references store(store_id));
 
-
+-- TODO
 grant select, insert, update, delete on {{database}}.customer_account to '{{sqlAuthUser}}'@'{{sqlHost}}';
+grant select, insert, update, delete on {{database}}.customer_account to '{{sqlVisitorUser}}'@'{{sqlHost}}';
 grant select, insert, update, delete on {{database}}.customer_account to '{{sqlCustomerUser}}'@'{{sqlHost}}';
+grant select, insert, update, delete on {{database}}.customer_account to '{{sqlEmployeeUser}}'@'{{sqlHost}}';
+grant select, insert, update, delete on {{database}}.customer_account to '{{sqlAdminUser}}'@'{{sqlHost}}';
