f1e548a31433c3a68618349160b7bc796d805539
brandon <wingo206bc@gmail.com>
2024-03-23 22:13:44 -0400

    refactor to use the point datatype, added a bunch of test data in the script

--- models/template_setup.sql
+++ models/template_setup.sql
@@ -24,10 +24,10 @@ create table customer_account(cid int primary key auto_increment, username varch
 create table admin_account(aid int primary key auto_increment, email varchar(40), password_hash varchar(50));
 insert into admin_account(email, password_hash) values ('admin@mrpizza.com', 'admin');
 
-create table store (store_id int primary key auto_increment, address varchar(40), latitude decimal(20, 15) not null, longitude decimal(20, 15) not null, image_url varchar(1000));
+create table store (store_id int primary key auto_increment, address varchar(40), latlng point not null, image_url varchar(1000));
 
-create table employee_account(eid int primary key auto_increment, name varchar(50), employee_type varchar(20), email varchar(50), password_hash varchar(50), works_at int,
-foreign key(works_at) references store(store_id));
+create table employee_account(eid int primary key auto_increment, name varchar(50), employee_type varchar(20), email varchar(50), password_hash varchar(50), status varchar(50), works_at int,
+foreign key(works_at) references store(store_id) on delete set null);
 insert into employee_account(name, email, password_hash) values ('mr pizza', 'employee1@mrpizza.com', 'employee1');
 
 create table help_ticket(tid int auto_increment primary key, asked_by int not null, answered_by int, date_created date, question varchar(200), answer varchar(200), quality_rating boolean, original_tid int, DTSTAMP datetime,
@@ -35,8 +35,8 @@ foreign key(asked_by) references customer_account(cid),
 foreign key(answered_by) references employee_account(eid));
 alter table help_ticket add constraint original_tid_references_help_ticket foreign key(original_tid) references help_ticket(tid) on delete set null;
 
-create table customer_order(order_id int primary key auto_increment, credit_card varchar(20), status varchar(20), total_price float, delivery_address varchar(40), DT_created datetime, DT_delivered datetime, ordered_by int not null, 
-foreign key(ordered_by) references customer_account(cid));
+create table customer_order(order_id int primary key auto_increment, credit_card varchar(20), status varchar(20), total_price float, delivery_latlng point, DT_created datetime, DT_delivered datetime, ordered_by int not null, 
+foreign key(ordered_by) references customer_account(cid) on delete cascade);
 
 create table menu_item(mid int primary key auto_increment, price float, image_url varchar(1000), description varchar(1000));
 
