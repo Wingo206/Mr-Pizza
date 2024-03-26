10f4791d7a4fc74e22d54bfbbcba66c2dc87e05c
Vineal Sunkara <vineal101@gmail.com>
2024-03-26 16:16:25 -0400

    removed some console.logs

--- public/order/pastOrderScript.js
+++ public/order/pastOrderScript.js
@@ -1,13 +1,13 @@
 
 import {cartEntry, populateCartTable, calculateTotalCost, displayCart} from './orderFunctions.js';
-const cart = [new cartEntry("pizza", 2, 11.99, 11.99 * 2), new cartEntry('wings', 1, 6.99, 6.99)];
-console.log(cart);
+// const cart = [new cartEntry("pizza", 2, 11.99, 11.99 * 2), new cartEntry('wings', 1, 6.99, 6.99)];
+// console.log(cart);
 let statusUpdated = new Array();
 
 // window.addEventListener('load', populateCartTable("#cart tbody", cart));
 // //there would be calls to database here instead
 let orders = await initialize();
-console.log(orders);
+// console.log(orders);
 window.addEventListener('load', displayOrders("#cart tbody", orders));
 
 // function displayOrders(query, orders) {
@@ -87,12 +87,11 @@ function displayOrders(query, orders) {
 
 // Fetch SQL datbase order items
 async function initialize() {
-     console.log('lol');
      const response = await fetch("/order/getPastOrders", {
         method: "GET",
       });
       const past_orders = await response.json();
-      console.log(past_orders);
+      //console.log(past_orders);
       return past_orders;
 }
 
