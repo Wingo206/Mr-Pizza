39881162fbc4f7ad64be4ed52a1c0930e5a8a448
brandon <wingo206bc@gmail.com>
2024-04-24 15:58:34 -0400

    frontend for chatbot

--- public/chatbot/chatbot.js
+++ public/chatbot/chatbot.js
@@ -4,4 +4,50 @@ window.addEventListener("load", async (event) => {
    })
    let chatWindow = await resp.text();
    document.body.innerHTML += chatWindow;
+   window.addChatResponse('Hi! I\'m Mr. Pizza. What would you like help with?')
+   // add listener for enter inside the input
+   let input = document.getElementById('chatInput');
+   input.addEventListener('keydown', (event) => {
+      if (event.key == 'Enter') {
+         window.submitChatMessage();
+      }
+   })
 })
+
+window.addChatResponse = (text) => {
+   let response = `<div class="responseDiv">
+            <div class="responseAvatarDiv">
+               <img src="/favicon.ico" alt="Avatar" class="avatar">
+            </div>
+            <div class="chatEntry response">
+               <p class="chatText response">${text}</p>
+            </div>
+         </div> `;
+   // add the new question to the chat
+   let chat = document.getElementById('chat');
+   chat.innerHTML += response;
+   chat.scrollTop = chat.scrollHeight;
+}
+
+window.addChatQuestion = (text) => {
+   let question = `<div class="chatEntry question">
+         <p class="chatText question">${text}</p>
+         </div> `
+   // add the new question to the chat
+   let chat = document.getElementById('chat');
+   chat.innerHTML += question;
+   chat.scrollTop = chat.scrollHeight;
+}
+
+window.submitChatMessage = async () => {
+   console.log("Sending chat message");
+   input = document.getElementById('chatInput');
+   let text = input.value;
+
+   //validate input question
+   if (text.length == 0) {
+      return;
+   }
+   input.value = '';
+   window.addChatQuestion(text);
+}
