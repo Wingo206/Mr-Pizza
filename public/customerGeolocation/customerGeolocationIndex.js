222caafcfb7bd9f107c2542fa0af30f7895e0298
daMon <dl1023@scarletmail.rutgers.edu>
2024-04-29 00:25:14 -0400

    wip geolocation integration

---
+++ public/customerGeolocation/customerGeolocationIndex.js
@@ -0,0 +1,16 @@
+let map;
+
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: 40.523421858838276, lng: -74.45823918823967 },
+        zoom: 15,
+        mapId: '5f088c2dddf9c012'
+    });
+
+    const control = document.getElementById("floating-panel");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+}
+
+initMap();
