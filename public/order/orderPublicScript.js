943602df5a22af41730e3e82b13afec48e1cf79f
AryaShetty08 <arya.shetty08@gmail.com>
2024-04-29 12:39:38 -0400

    added total order to order page and fixed tip button

--- public/order/orderPublicScript.js
+++ public/order/orderPublicScript.js
@@ -32,6 +32,14 @@ console.log("This is isDelivery: " + isDelivery);
   total = total.toFixed(2);
 
   populateCartTable('#cart tbody', cartEntries2);
+
+  // Optionally, add a final row for the total price of all items
+  const tableBody = document.querySelector("#cart tbody");
+  const totalRow = tableBody.insertRow();
+  const totalCell = totalRow.insertCell();
+  totalCell.textContent = "Order Price: " + total;
+  totalCell.colSpan = 5; // Span across all columns
+
   const totalCost = calculateTotalCost(cartEntries2);
   console.log('Total cost:', totalCost);
 
